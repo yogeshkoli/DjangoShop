d6fe40ca2efde255f604932cd715f92221f7c3d1
Yogesh Koli <yogeshkoli@hotmail.com>
2023-04-25 17:26:50 +0530

    feat: integration of statifiles & created templates page along with the base layout/master page

--- DjangoShop/settings.py
+++ DjangoShop/settings.py
@@ -123,6 +123,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [
+    "DjangoShop/staticfiles",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
