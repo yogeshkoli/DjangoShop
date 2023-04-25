ebaee25d09e6ecf03de30863b967e05b845cda51
Yogesh Koli <yogeshkoli@hotmail.com>
2023-04-25 17:36:04 +0530

    feat: setup model & register model to admin

--- category/models.py
+++ category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    image = models.ImageField(upload_to='uploads/categories', blank=True)
+
+    def __str__(self) -> str:
+        return self.category_name
