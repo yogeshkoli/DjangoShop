bbfe490707410dcecfd0fd9ea50b002d0fd3aeed
Yogesh Koli <yogeshkoli@hotmail.com>
2023-04-26 16:14:09 +0530

    fix: updates to migration

--- category/models.py
+++ category/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.CharField(max_length=100, unique=True)
     description = models.TextField(max_length=255, blank=True)
-    image = models.ImageField(upload_to='uploads/categories', blank=True)
+    image = models.ImageField(upload_to='categories/', blank=True)
 
     class Meta:
         verbose_name = 'category'
